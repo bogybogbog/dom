67f634184cc256e61949f054cd3205c468c62ef6
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2024-09-21 01:14:20 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+// var x = document.querySelector('')
+// x.addEventListener()
+
+// ! EVENTS :{
+
+// 1- click
+// 2- dblclick
+// 3- contextmenu ====> right click
+// 4- mousemove
+// 5- mouseup
+// 6- mousedown
+// 7- mouseenter ====> when my mouse enters the area
+// 8- mouseleave ====> when my mouse leaves the area
+// 9- drag ====> when u drag the img or anythibg u can drag and also while dragging the function will be excuted
+// 10- keyup
+// 11- keydown
+// 12- keypress ====> writing keys
+// 13- for inputs : 1- input ===> changing the value of the input
+//                  2- focus ===>
+//                  3- blur  ===> the oppsite of focus
+// 14- submit ====> it makes the website reloads and its not good so we prevent that action by using preventDefault() :
+// var x = document.querySelector("form");
+// document.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   console.log("hii");
+// });
+// 15-
+// var x = document.querySelector("img");
+// document.addEventListener("click", function (e) {
+//  var x = e.target.getAttribute('src');   ====> getting any attribute
+//  var x = e.target.setAttribute('src', "images/1.jpg");   ====> setting any attribute
+//   console.log("x");
+// });
+// 16-
+// 17-
+
+// ! }
+
+// var x = document.querySelector('')
+// document.addEventListener('keydown', function(e){
+//     console.log(e);
+// })
+
+// var x = document.querySelectorAll(".item");
+// for (var i = 0; i < x.length; i++) {
+//   x[i].addEventListener("click", function (e) {
+//     if (e.target.classList.contains("bg-danger")) {
+//       e.target.classList.replace("bg-danger", "bg-info");
+//     } else {e.target.classList.replace("bg-info", "bg-danger");}
+//   });
+// }
+
+// for (var i = 0; i < 50; i++) {
+//   var ele = document.createElement("img");
+
+//   ele.setAttribute("src", "css/chefs-1.jpg");
+//   ele.setAttribute("class", "w-25");                    =====> adding elements in html
+//   ele.setAttribute("alt", "de7k");
+//   ele.classList.add("rounded-circle", "test", "");
+//   document.body.appendChild(ele);
+// }
